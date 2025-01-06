6e3af2c915e669215de462d9f5e5396d781ac660
Peleda23 <kosmosas23@gmail.com>
2025-01-06 14:42:29 +0200

    [80]

---
+++ company/pages/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from .views import home_page_view, AboutPageView
+
+urlpatterns = [path("about/", AboutPageView.as_view()), path("", home_page_view)]
--- company/pages/views.py
+++ company/pages/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 def home_page_view(request):
-    return render(request, "home.html")
+    context = {
+        "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
+        "greeting": "THAnk you FOR visitING",
+    }
+    return render(request, "home.html", context)
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["contact"]
